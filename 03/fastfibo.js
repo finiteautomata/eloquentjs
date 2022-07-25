d7faa5f96f96608d92fd620593ec96fb58eb50bb
Juan Manuel Pérez <jmperez.85@gmail.com>
2022-07-25 11:30:42 -0300

    Capítulo 3

---
+++ 03/fastfibo.js
@@ -0,0 +1,17 @@
+const fastFibo = (n) => {
+    let count = 2;
+    let prev=1, curr = 1;
+    while (count < n) {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+        ++count;
+    }
+
+    return curr
+}
+
+
+for (let c = 0; c < 15; c+=1) {
+    console.log(`${c} --> ${fastFibo(c)}`)
+}
